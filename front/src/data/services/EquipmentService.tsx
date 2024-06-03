cbdbda7501ffa1a1748010259deb6995adeff2c5
Marie <mje.espinosa@gmail.com>
2024-06-03 13:57:47 +0200

    improve equipements function and reorder list

--- front/src/data/services/EquipmentService.tsx
+++ front/src/data/services/EquipmentService.tsx
@@ -1,18 +1,16 @@
+import { reorderList } from "../../utils/utils";
 import { database } from "../firebase/firebase";
 
 const EquipementsService = () => {
   const getEquipments = async () => {
     try {    
-    const equipmentsRef = database.ref('Equipments');
-    const snapshot = await equipmentsRef.get();
-
-    if (snapshot.exists()) {
+    const equipmentsRef = database.ref('Equipments').once('value').then(function (snapshot) {           
       const data = snapshot.val();
       const equipmentsArray = Object.keys(data).map(key => ({ id: key, ...data[key] }));
-      return equipmentsArray;
-    } else {
-      return [];
-    }
+      const finalList = reorderList(equipmentsArray)
+      return finalList;
+    });
+    return equipmentsRef
     } catch (error) {
       throw new Error('Failed to fetch equipments');
     }
