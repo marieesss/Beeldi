45e4df451044486ea55ba6840cda895f9c9603a7
Marie <mje.espinosa@gmail.com>
2024-06-04 13:26:51 +0200

    add icons

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,20 +1,20 @@
 import React from "react";
 import "./App.css";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import EquipmentPage from "./screens/Equipement/Equipment";
 import Header from "./components/Header/Header";
 import EquipementDetails from "./screens/EquipementDetails/EquipementDetails";
 
 function App() {
   return (
-    <>
-      <Header />
-      <Router>
+    <> 
+      <BrowserRouter>
+        <Header/>
         <Routes>
           <Route path="/" element={<EquipmentPage />} />
           <Route path="/:id" element={<EquipementDetails />} />
         </Routes>
-      </Router>
+      </BrowserRouter>
     </>
   );
 }
